dcd0ff617f85992bcdc81ff2d533ab0085b18608
Ruzain <muhammadruzain10@gmail.com>
2025-07-10 00:07:48 +0700

    feat: add game resources UI

---
+++ src/components/Resource.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Resource: React.FC = () => {
+  const gold = 22194;
+  const time = 579;
+
+  return (
+    <div className="w-full h-fit flex justify-end items-center gap-x-5 ">
+      <div className="relative w-3/10 h-fit flex justify-end items-center">
+        <img className="" src="src/assets/containers/resource-time.svg" alt="" />
+        <span className="absolute right-0 font-bold text-xl px-6">{time}</span>
+      </div>
+
+      <div className="relative w-3/10 h-fit flex justify-end items-center ">
+        <img className="" src="src/assets/containers/resource-gold.svg" alt="" />
+        <span className="absolute right-0 font-bold text-xl px-6">{gold.toLocaleString()}</span>
+      </div>
+
+    <button className=" hover:scale-105">
+      <img
+        src="src/assets/game-icons/setting.svg"
+        alt="Settings"
+        className="w-10"
+      />
+    </button>
+    </div>
+  );
+};
+
+export default Resource;
--- src/pages/Homescreen.tsx
+++ src/pages/Homescreen.tsx
@@ -1,21 +1,29 @@
+import Resource from "../components/Resource"
 
 function Homescreen() {
 
   return (
     <>
 
-      <div className="w-screen h-screen bg-cover bg-bottom-left bg-[url(src/assets/homescreen-bg.png)]">
+      <div className="w-screen h-screen bg-cover bg-bottom-left bg-[url(src/assets/screens/homescreen-bg.png)]">
             <div className="absolute w-screen h-screen bottom-0 left-0 overflow-hidden">
-              <img className="absolute -bottom-0 move-animation" style={{ '--duration': '2' } as React.CSSProperties} src="src/assets/cloud/cloud1.png"></img>
-              <img className="absolute -bottom-0 move-animation" style={{ '--duration': '3' } as React.CSSProperties} src="src/assets/cloud/cloud1.png"></img>
-              <img className="absolute -bottom-0 move-animation" style={{ '--duration': '5' } as React.CSSProperties} src="src/assets/cloud/cloud2.png"></img>
-              <img className="absolute -bottom-5 move-animation" style={{ '--duration': '7' } as React.CSSProperties} src="src/assets/cloud/cloud3.png"></img>
-              <img className="absolute -bottom-0 move-animation" style={{ '--duration': '11' } as React.CSSProperties} src="src/assets/cloud/cloud2.png"></img>
-              <img className="absolute -bottom-5 move-animation" style={{ '--duration': '13' } as React.CSSProperties} src="src/assets/cloud/cloud3.png"></img>
+              <img className="absolute -bottom-0 move-animation" style={{ '--duration': '2' } as React.CSSProperties} src="src/assets/clouds/cloud1.png"></img>
+              <img className="absolute -bottom-0 move-animation" style={{ '--duration': '3' } as React.CSSProperties} src="src/assets/clouds/cloud1.png"></img>
+              <img className="absolute -bottom-0 move-animation" style={{ '--duration': '5' } as React.CSSProperties} src="src/assets/clouds/cloud2.png"></img>
+              <img className="absolute -bottom-5 move-animation" style={{ '--duration': '7' } as React.CSSProperties} src="src/assets/clouds/cloud3.png"></img>
+              <img className="absolute -bottom-0 move-animation" style={{ '--duration': '11' } as React.CSSProperties} src="src/assets/clouds/cloud2.png"></img>
+              <img className="absolute -bottom-5 move-animation" style={{ '--duration': '13' } as React.CSSProperties} src="src/assets/clouds/cloud3.png"></img>
             </div>
 
-            <div className="absolute w-screen h-screen bg-contain bg-bottom-right bg-no-repeat bg-[url(src/assets/homescreen-fg.png)]">
-
+            <div className="absolute w-screen h-screen bg-contain bg-bottom-right bg-no-repeat bg-[url(src/assets/screens/homescreen-fg.png)]">
+              <div className="w-screen h-screen px-16 py-8 flex flex-col justify-between items-center">
+                <div className="w-full h-1/18 flex justify-end">
+                  <div className="w-1/2 h-full">
+                    <Resource></Resource>
+                  </div>
+                </div>
+                <div className="w-1/2 h-1/6 bg-amber-300"></div>
+              </div>
             </div>
           
 
