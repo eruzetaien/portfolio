f9802ea6aad800e3ef2c7802deb7649574bc9965
Ruzain <muhammadruzain10@gmail.com>
2025-07-11 21:56:40 +0700

    feat: add education page

--- src/pages/EducationPage.tsx
+++ src/pages/EducationPage.tsx
@@ -1,13 +1,27 @@
+import BackButton from "../components/BackButton"
+import Resource from "../components/Resource"
 
 function EducationPage() {
 
   return (
     <>
-      <div className="w-screen h-screen flex justify-center items-center">
-        <h1> Education Page</h1>
+      <div className="w-screen h-screen flex flex-col items-center bg-gradient-to-b from-[#495891] to-[#B9C4DA]">
+        <div className="w-full px-16 py-8 flex justify-between items-center">
+            <div>
+              <BackButton pageName="Education" />
+            </div>
+            <div className="w-1/2 h-full">
+              <Resource/>
+            </div>
+        </div>
+
+        <div className="w-full h-full px-16 pb-4 flex flex-col justify-between items-center gap-y-5 overflow-auto [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
+          <div className="flex w-full items-start justify-center pt-5 pb-10 gap-x-12">
+            <img className="w-1/5" src="src/assets/bangkit-banner.svg" alt=""/>
+            <img className="w-1/4" src="src/assets/ui-banner.svg" alt=""/>
+          </div>
+        </div>
       </div>
-
-
     </>
   )
 }
