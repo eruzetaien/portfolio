a7a8a192e2b4d1c4db0e1e5b083fd18aa58b2e54
Ruzain <muhammadruzain10@gmail.com>
2025-07-13 21:32:44 +0700

    feat: add incremental mechanism for gold and time game resource

--- src/components/Resource.tsx
+++ src/components/Resource.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Setting from "./Setting";
 
 const Resource: React.FC = () => {
-  const gold = 22194;
-  const time = 106;
+  const initialGold = 22194;
+  const earningPerHour = 0;
+  const [gold, setGold] = useState(initialGold);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const update = () => {
+      const now = new Date();
+      const totalMinutes = now.getHours() * 60 + now.getMinutes(); // 0 to 1439
+      const goldValue =  initialGold + now.getHours() * earningPerHour  ; // increments each hour
+      setTime(totalMinutes);
+      setGold(goldValue);
+    };
+
+    update(); // initial run
+    const interval = setInterval(update, 60 * 1000); // update every 1 minute
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="w-full h-fit flex justify-end items-center gap-x-5 ">
